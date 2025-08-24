f4ddef728e1d8a866f9b2200619f6a6e6fde0402
alexiswilliams010 <204508632+alexiswilliams010@users.noreply.github.com>
2025-08-24 18:03:09 -0400

    Add autogenerated Solidity ql, db schema, and tools for codeql db creation

---
+++ tools/qltest.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -eu
+
+exec "${CODEQL_DIST}/codeql" database index-files \
+    --prune="**/*.testproj" \
+    --include-extension=.sol \
+    --size-limit=5m \
+    --language=solidity \
+    --working-dir=.\
+    "$CODEQL_EXTRACTOR_SOLIDITY_WIP_DATABASE"
